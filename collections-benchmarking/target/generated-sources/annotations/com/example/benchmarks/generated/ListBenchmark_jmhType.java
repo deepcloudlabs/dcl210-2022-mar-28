cdaa23b26fe3300c4f830ae0d17082b23c78f0a0
deepcloudlabs <deepcloudlabs@gmail.com>
2022-03-31 19:03:12 +0300

    initial commit

--- collections-benchmarking/target/generated-sources/annotations/com/example/benchmarks/generated/ListBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.example.benchmarks.generated;
-public class ListBenchmark_jmhType extends ListBenchmark_jmhType_B3 {
-}
-
--- collections-benchmarking/target/generated-sources/annotations/com/example/benchmarks/generated/SetBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.example.benchmarks.generated;
-public class SetBenchmark_jmhType extends SetBenchmark_jmhType_B3 {
-}
-
